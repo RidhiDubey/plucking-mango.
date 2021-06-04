0ce9dfcf81b5182c5cad71b29b8f8d435b7a973c
RidhiDubey <76939922+RidhiDubey@users.noreply.github.com>
2021-06-04 12:48:46 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,34 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+
+        
+        this.pointB = pointB
+        this.launcher = Constraint.create(options);
+        World.add(world, this.launcher);
+    }
+
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+    
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+        
+        stroke(48,22,8);
+        
+
+            push();
+            pop();
+        
+    }
+    
+}
